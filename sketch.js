ae00fa1d58513da138bdeae54fb55121ee2a2782
marinamoore <mmoore32@calpoly.edu>
2016-11-12 15:15:03 -0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var screenSize;
+var number;
+var circleSize;
+var radius
+
+var dictionary = {};
+var circleLocations = [];
+var color = {};
+var img;
+
+function preload(){
+   //img = loadImage("assets/puppy.jpg");
+}
+
+function setup() {
+   screenSize = 600;
+   circleSize = 400;
+   number = 20;
+   radius = 50;
+
+   createCanvas(screenSize, screenSize);
+   for(i = 0; i < number; i++) {
+      dictionary[i] = [];
+      color[i] = [255, 255/number * i, 203];
+   }
+}
+
+function convertX(ndx) {
+   return screenSize/2 + circleSize/2 * cos(ndx * TWO_PI/number);
+}
+
+function convertY(ndx) {
+   return screenSize/2 + circleSize/2 * sin(ndx * TWO_PI/number);
+}
+
+function addLine(ndx1, ndx2) {
+   dictionary[ndx1].push(ndx2);
+   dictionary[ndx2].push(ndx1);
+   x1 = convertX(ndx1);
+   y1 = convertY(ndx1);
+   x2 = convertX(ndx2);
+   y2 = convertY(ndx2);
+   line(x1, y1, x2, y2);
+}
+
+function draw() {
+   addLine(0, 5);
+   addLine(0, 10);
+   addLine(1, 8);
+   for(i = 0; i < number; i++) {
+      if (dictionary[i].length > 1) {
+         color[i] = [0, 255, 255];
+      }
+      x = convertX(i);
+      y = convertY(i);
+      circleLocations.push([x, y]);
+      fill(color[i][0], color[i][1], color[i][2]);
+      ellipse(x,y, radius, radius);
+   }
+
+   //image(img, 0, 0, puppy.width, puppy.height, 0, 0, 300);
+  
+}
+
+function mouseClicked() {
+   for (i = 0; i < circleLocations.length; i++) {
+      if(dist(mouseX, mouseY, circleLocations[i][0], circleLocations[i][1]) < radius/2) {
+         color[i] = [255/number * i, 0, 255];
+      }
+   }
+}
