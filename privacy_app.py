299bb4d944282458ba2ef816292f2b06f2b14cab
Max Zinkus <mzinkus@calpoly.edu>
2016-12-01 12:20:27 -0800

    Add d3 demo

--- privacy_app.py
+++ privacy_app.py
@@ -1,21 +1,27 @@
-
+from os import environ
 from flask import Flask, request, render_template, session, redirect
 
 import sqlite3
 conn = sqlite3.connect('app.db')
 
 app = Flask(__name__)
+app.secret_key = environ['SECRET_KEY']
 debug = True
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('index.html', authed="username" in session)
    #return render_template('home.html')
+
+@app.route('/d3/')
+def demo():
+   return render_template('demo.html')
 
 @app.route('/login/', methods=["GET", "POST"])
 def login():
    username = request.form['name']
    password = request.form['password']
+   session['username'] = username
    return render_template("login.html", u=username, p=password)
 
 if __name__ == '__main__':
