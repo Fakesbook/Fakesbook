8abe0bd78ed5b63ff68f0cc236b2a9955959bd5a
Max Zinkus <mzinkus@calpoly.edu>
2016-12-01 00:23:25 -0800

    Start the login functionality

--- privacy_app.py
+++ privacy_app.py
@@ -1,12 +1,22 @@
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect
+
+import sqlite3
+conn = sqlite3.connect('app.db')
 
 app = Flask(__name__)
 debug = True
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
+   #return render_template('home.html')
+
+@app.route('/login/', methods=["GET", "POST"])
+def login():
+   username = request.form['name']
+   password = request.form['password']
+   return render_template("login.html", u=username, p=password)
 
 if __name__ == '__main__':
     app.run(debug=debug, port=8080)    
---
+++ static/loginModal.js
@@ -0,0 +1,11 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("loginSubmit");
+
+modal.style.display = "block";
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "none";
+}
