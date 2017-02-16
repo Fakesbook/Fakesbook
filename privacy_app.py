f9595603a18015df786bd5072b2e4475b2194a4f
Max Zinkus <mzinkus@calpoly.edu>
2017-02-16 08:31:42 -0800

    Login, register, homepage, user page
    Corrected the redirects from login->register
    Update statement in SQL for account setup
    Put the graph in an iframe on the homepage
    Added secure password storage via bcrypt

--- privacy_app.py
+++ privacy_app.py
@@ -1,20 +1,12 @@
 from os import environ, urandom
 from hmac import HMAC as hmac, compare_digest 
+import bcrypt
 from hashlib import sha256
 from base64 import b64encode, b64decode
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, flash
 
 import sqlite3
 conn = sqlite3.connect('app.db', check_same_thread=False)
-
-def compare_passwords(a, b):
-   """ VERY safely compare passwords """
-   try:
-      pw = b64encode(urandom(32))
-      return compare_digest(hmac(pw, a.encode(), sha256).hexdigest(),
-            hmac(pw, b.encode(), sha256).hexdigest())
-   except UnicodeEncodeError:
-      return False
 
 c = conn.cursor()
 c.execute("""DROP TABLE IF EXISTS User""")
@@ -29,8 +21,6 @@ c.execute("""
       phone text defaul null,
       fav_color text default null
    )""")
-
-c.execute("""insert into User(username, password) values ('max', 'foo')""")
 conn.commit()
 
 app = Flask(__name__)
@@ -42,40 +32,68 @@ def home():
    return render_template('index.html', authed="username" in session)
 
 @app.route('/d3/')
-def demo():
-   return render_template('demo.html')
+def graph():
+   c = conn.cursor()
+   users = c.execute("""SELECT username,gender,image,birthdate,phone,fav_color FROM User""").fetchall()
+   users.sort(lambda u: u.username)
+   friends = {0:1} # TODO create demo friend connections, then real ones
+   # TODO put example values back in templates/demo.html to see if iframe looks good
+   return render_template('demo.html', users=users, len=len(users), friends=friends)
 
 @app.route('/login/', methods=["GET", "POST"])
 def login():
-   if request.method == 'POST':
-      username = request.form['name']
+   if request.method == 'POST' and "username" not in session:
+      username = request.form['name'].capitalize()
       password = request.form['password']
       user_pw = c.execute("""
          SELECT password FROM User
          WHERE username=? LIMIT 1""", (username,)).fetchone()
-      if user_pw and compare_passwords(password, user_pw[0]):
+      if user_pw and bcrypt.checkpw(password.encode('utf-8'), user_pw[0]):
          session['username'] = username
-         return render_template("login.html", u=username, p=password)
+      else:
+         flash("Incorrect username/password")
    return redirect('/')
 
-@app.route('/accountsetup/', methods=["POST"])
+@app.route('/accountsetup/', methods=["GET", "POST"])
 def accountsetup():
-    gender = request.form['gender']
-    favcolor = request.form['color']
-    birthday = request.form['birthday']
-    age = request.form['age']
-    phone = request.form['phone']
-    c = conn.cursor()
-    c.execute("""INSERT INTO User(gender, birthdate, phone, fav_color) values (?, ?, ?, ?)""", (gender, birthdat, phone, favcolor))
-    return redirect("/login")
+    if not "username" in session:
+        return redirect('/')
+    if request.method == "POST":
+        gender = request.form['gender']
+        favcolor = request.form['color']
+        birthday = request.form['birthday']
+        age = request.form['age']
+        phone = request.form['phone']
+        c = conn.cursor()
+        c.execute("""UPDATE User SET gender=?,birthdate=?,phone=?,fav_color=? WHERE username=?""", (gender,birthday,phone,favcolor,session['username']))
+        conn.commit()
+        return redirect("/")
+    return render_template("createaccount.html")
+
+@app.route('/addfriend/', methods=["POST"])
+def addfriend():
+    if not "username" in session:
+       return redirect("/")
+    target = request.form
+    # TODO
 
 @app.route('/register/', methods=["POST"])
 def register():
-   username = request.form['name']
+   if "username" in session:
+      flash("You're already logged in!")
+      return redirect('/')
+   username = request.form['name'].capitalize()
    password = request.form['password']
    c = conn.cursor()
-   c.execute("""INSERT INTO User(username, password) VALUES (?, ?)""", (username, password))
+   user = c.execute("""
+         SELECT username FROM User
+         WHERE username=? LIMIT 1""", (username,)).fetchone()
+   if user:
+      flash("Username is already taken.")
+      return redirect("/")
+   c.execute("""INSERT INTO User(username, password) VALUES (?, ?)""", (username, bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())))
    conn.commit()
+   session['username'] = username
    return redirect("/accountsetup/")
 
 @app.route('/logout/')
@@ -83,11 +101,11 @@ def logout():
    session.pop("username")
    return redirect("/")
 
-@app.route('/users/<name>/')
+@app.route('/user/<name>/')
 def user_info(name):
+    c = conn.cursor()
     user = c.execute("""SELECT * from User where username=? LIMIT 1""",
-                        (name,)).fetchone()
-    print(user) # TODO remove
+                        (name.capitalize(),)).fetchone()
     return render_template("user.html", user=user)
 
 if __name__ == '__main__':
