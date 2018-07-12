1d6654f3d88d8b1f355aada22257b47c88687041
Oliver Curry <owcurry42@gmail.com>
2018-07-11 19:31:05 -0700

    added solution to ceasar cipher

---
+++ ceasar.py
@@ -0,0 +1,26 @@
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t" "u", "v", "w", "x", "y", "z"]
+
+def encode(s, offset):
+    out = ""
+    for c in s:
+        if c in alphabet:
+            out += alphabet[(alphabet.index(c)+offset) % 26]
+        else:
+            out += c
+    return out
+
+def decode(s, offset):
+    out = ""
+    for c in s:
+        if c in alphabet:
+            out += alphabet[(alphabet.index(c) - offset) % 26]
+        else:
+            out += c
+    return out
+
+if __name__ == '__main__':
+    code = encode("hello world", -1)
+    print(code)
+    out = decode(code, -1)
+    print(out)
