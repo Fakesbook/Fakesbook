bbcb2e0a34240e3953980cc91ca74f9f45cd8055
Oliver Curry <owcurry42@gmail.com>
2019-04-02 20:33:14 -0700

    added ability to launch and stop the server from the gui

--- fakesbook.py
+++ fakesbook.py
@@ -2,29 +2,38 @@ from os import environ, urandom
 from sys import argv
 from base64 import b64encode
 from privacy_app import app
-from twisted.internet import reactor
+from twisted.internet import tksupport, reactor
 from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 from appJar import gui
 
-if __name__ == '__main__':
+port = 8080
+debugPort = 8081
 
-    appgui = gui()
+def launchApp():
+    flask_site = WSGIResource(reactor, reactor.getThreadPool(), app)
+    reactor.listenTCP(port, Site(flask_site))
+    reactor.run()
+
+def stopApp():
+    reactor.stop()
+
+def launchDebug():
+    app.run(debug=True, port=debugPort)
+
+def buildGUI():
+    appgui = gui("Fakesbook", "400x400")
+
+    tksupport.install(appgui.topLevel)
 
     appgui.addLabel("title", "Fakesbook")
-    appgui.setLabelBg("title", "red")
+    appgui.buttons(["Launch", "Stop"], [launchApp, stopApp])
 
     appgui.go()
 
-
-    """
-    port = 8080
-    debugPort = 8081
+if __name__ == '__main__':
 
     if len(argv) > 1 and argv[1] == "debug":
-        app.run(debug=True, port=debugPort)
+        launchDebug()
     else:
-        flask_site = WSGIResource(reactor, reactor.getThreadPool(), app)
-        reactor.listenTCP(port, Site(flask_site))
-        reactor.run()
-        """
+        buildGUI()
