13f368372b816ec7b6fa26cfe020a3f22c03cd38
Oliver Curry <owcurry42@gmail.com>
2019-04-03 19:43:58 -0700

    added ip address

--- fakesbook.py
+++ fakesbook.py
@@ -6,9 +6,21 @@ from twisted.internet import tksupport, reactor
 from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 from appJar import gui
+import socket
 
 port = 8080
 debugPort = 8081
+
+def getIPAddress():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.connect(('10.255.255.255', 1))
+        ip = sock.getsockname()[0]
+    except:
+        ip = socket.gethostbyname(socket.gethostname())
+    finally:
+        sock.close()
+    return ip
 
 def launchApp():
     flask_site = WSGIResource(reactor, reactor.getThreadPool(), app)
@@ -21,13 +33,22 @@ def stopApp():
 def launchDebug():
     app.run(debug=True, port=debugPort)
 
+def saveDatabase():
+    pass
+
+def loadDatabase():
+    pass
+
 def buildGUI():
     appgui = gui("Fakesbook", "400x400")
 
     tksupport.install(appgui.topLevel)
 
     appgui.addLabel("title", "Fakesbook")
+    appgui.buttons(["Save Database", "Load Database"], [saveDatabase, loadDatabase])
     appgui.buttons(["Launch", "Stop"], [launchApp, stopApp])
+
+    appgui.addLabel("address", getIPAddress())
 
     appgui.go()
 
